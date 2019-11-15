b8fc04d741296105c75efa5a4f56e26405cd9261
harrychen1995 <harrychen1995@yahoo.com>
2019-11-15 17:34:01 -0500

    bug fixed

--- Predict_Image.py
+++ Predict_Image.py
@@ -1,10 +1,7 @@
 import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
 import tensorflow as tf
-import zipfile
 from collections import Counter
 from io import StringIO
 from matplotlib import pyplot as plt 
